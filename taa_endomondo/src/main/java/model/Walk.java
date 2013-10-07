66b68e5fa072d37ca8d85b2131b8dcbb4e52d4aa
11009889 <11009889@i210m04.istic.univ-rennes1.fr>
2013-10-07 16:16:29 +0200

    2

---
+++ taa_endomondo/src/main/java/model/Walk.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Collection;
+
+public class Walk {
+  
+	private int id;
+	private Collection<CoordGPS> coordListGps;
+	
+	
+}
--- taa_endomondo/src/main/java/model/Weather.java
+++ taa_endomondo/src/main/java/model/Weather.java
@@ -4,10 +4,11 @@ public class Weather {
 
 	private int id;
 	
-	// TODO: REF http://developer.yahoo.com/weather/ complet it today or tomorrow 2013-10-07
+	// TODO: REF http://developer.yahoo.com/weather/ complet it today or tomorrow 2013-10-07, see more details about the temperature attributes
 
 	private String location;
 	private int temperature;
+	
 
 	 
 }
